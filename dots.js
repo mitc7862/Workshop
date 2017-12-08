2cb5d8e36667a3cbdefc19f576aee293cfb0096e
Mitchel Cox <mitchelcox@alum.calarts.edy>
2017-12-08 11:27:31 -0800

    added dots experiment

---
+++ dots.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    $("#circle1").attr("cx", Math.floor(Math.random() * 960) + 1);
+    $("#circle1").attr("cy", Math.floor(Math.random() * 960) + 1);
+    $("#circle2").attr("cx", Math.floor(Math.random() * 960) + 1);
+    $("#circle2").attr("cy", Math.floor(Math.random() * 960) + 1);
+    $("#circle3").attr("cx", Math.floor(Math.random() * 960) + 1);
+    $("#circle3").attr("cy", Math.floor(Math.random() * 960) + 1);
+    $("#circle4").attr("cx", Math.floor(Math.random() * 960) + 1);
+    $("#circle4").attr("cy", Math.floor(Math.random() * 960) + 1);
+    $("#circle5").attr("cx", Math.floor(Math.random() * 960) + 1);
+    $("#circle5").attr("cy", Math.floor(Math.random() * 960) + 1);
+    $("#circle6").attr("cx", Math.floor(Math.random() * 960) + 1);
+    $("#circle6").attr("cy", Math.floor(Math.random() * 960) + 1);
+    $("#circle7").attr("cx", Math.floor(Math.random() * 960) + 1);
+    $("#circle7").attr("cy", Math.floor(Math.random() * 960) + 1);
+    $("#circle8").attr("cx", Math.floor(Math.random() * 960) + 1);
+    $("#circle8").attr("cy", Math.floor(Math.random() * 960) + 1);
+    $("#circle9").attr("cx", Math.floor(Math.random() * 960) + 1);
+    $("#circle9").attr("cy", Math.floor(Math.random() * 960) + 1);
+    $("#circle10").attr("cx", Math.floor(Math.random() * 960) + 1);
+    $("#circle10").attr("cy", Math.floor(Math.random() * 960) + 1);
+  
+    $("circle").mouseover(function() {
+      $("#circle1").attr("cx", Math.floor(Math.random() * 960) + 1);
+      $("#circle1").attr("cy", Math.floor(Math.random() * 960) + 1);
+      $("#circle2").attr("cx", Math.floor(Math.random() * 960) + 1);
+      $("#circle2").attr("cy", Math.floor(Math.random() * 960) + 1);
+      $("#circle3").attr("cx", Math.floor(Math.random() * 960) + 1);
+      $("#circle3").attr("cy", Math.floor(Math.random() * 960) + 1);
+      $("#circle4").attr("cx", Math.floor(Math.random() * 960) + 1);
+      $("#circle4").attr("cy", Math.floor(Math.random() * 960) + 1);
+      $("#circle5").attr("cx", Math.floor(Math.random() * 960) + 1);
+      $("#circle5").attr("cy", Math.floor(Math.random() * 960) + 1);
+      $("#circle6").attr("cx", Math.floor(Math.random() * 960) + 1);
+      $("#circle6").attr("cy", Math.floor(Math.random() * 960) + 1);
+      $("#circle7").attr("cx", Math.floor(Math.random() * 960) + 1);
+      $("#circle7").attr("cy", Math.floor(Math.random() * 960) + 1);
+      $("#circle8").attr("cx", Math.floor(Math.random() * 960) + 1);
+      $("#circle8").attr("cy", Math.floor(Math.random() * 960) + 1);
+      $("#circle9").attr("cx", Math.floor(Math.random() * 960) + 1);
+      $("#circle9").attr("cy", Math.floor(Math.random() * 960) + 1);
+      $("#circle10").attr("cx", Math.floor(Math.random() * 960) + 1);
+      $("#circle10").attr("cy", Math.floor(Math.random() * 960) + 1);
+    });
+  });
+  
